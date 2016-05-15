9160f17b11a9deeb8182c7692893676f686bba3d
Kris Stasik <krzysiek.stasik@gmail.com>
2016-05-15 16:03:47 -0400

    * changed to use base logs

--- link/src/common/link_private.h
+++ link/src/common/link_private.h
@@ -8,42 +8,53 @@
 
 struct LinkInterface;
 
-// Version of the link interface that allows 
+// Version of the link interface that allows
 // to detect interface compatibility.
 #define LINK_INTERFACE_VERSION 1
 
 // Link exposed functions.
-typedef int (*GetNotificationPtr)(void* context, struct Notification* notif, int timeout_ms);
-typedef WatchHandle (*CreatePluginWatchPtr)(void* context, const char* name_filter, const char* version_filter, const char* hostname_filter);
-typedef ConnectionHandle (*ConnectPtr)(void* context, PluginHandle handle);
-typedef int (*SendPtr)(void* context, ConnectionHandle connection, const void* bytes, streamsize count);
-typedef int (*GetInfoPtr)(void* context, PluginHandle handle, struct PluginInfo* info);
-typedef void (*LogWritePtr)(void* context, const char* file, int line, int category, const char* format, va_list args);
-typedef unsigned int (*GetTotalRecvAvailablePtr)(void* context, ConnectionHandle conn);
-typedef unsigned int (*GetRecvAvailablePtr)(void* context, ConnectionHandle conn);
-typedef int (*RecvPtr)(void* context, ConnectionHandle handle, void* buffer, unsigned int nbytes);
-typedef void (*CloseConnectionPtr)(void* context, ConnectionHandle conn);
-typedef void (*CloseWatchPtr)(void* context, WatchHandle watch);
-typedef PluginHandle (*RegisterPluginPtr)(void* context, struct PluginInfo* info);
-typedef void (*UnregisterPluginPtr)(void* context, PluginHandle handle);
-typedef struct LinkConfiguration (*GetConfigurationPtr)(void* context);
+typedef int (*GetNotificationPtr)(void *context, struct Notification *notif,
+                                  int timeout_ms);
+typedef WatchHandle (*CreatePluginWatchPtr)(void *context,
+                                            const char *name_filter,
+                                            const char *version_filter,
+                                            const char *hostname_filter);
+typedef ConnectionHandle (*ConnectPtr)(void *context, PluginHandle handle);
+typedef int (*SendPtr)(void *context, ConnectionHandle connection,
+                       const void *bytes, streamsize count);
+typedef int (*GetInfoPtr)(void *context, PluginHandle handle,
+                          struct PluginInfo *info);
+typedef void (*LogWritePtr)(void *context, const char *file, int line,
+                            int category, const char *format, va_list args);
+typedef unsigned int (*GetTotalRecvAvailablePtr)(void *context,
+                                                 ConnectionHandle conn);
+typedef unsigned int (*GetRecvAvailablePtr)(void *context,
+                                            ConnectionHandle conn);
+typedef int (*RecvPtr)(void *context, ConnectionHandle handle, void *buffer,
+                       unsigned int nbytes);
+typedef void (*CloseConnectionPtr)(void *context, ConnectionHandle conn);
+typedef void (*CloseWatchPtr)(void *context, WatchHandle watch);
+typedef PluginHandle (*RegisterPluginPtr)(void *context,
+                                          struct PluginInfo *info);
+typedef void (*UnregisterPluginPtr)(void *context, PluginHandle handle);
+typedef struct LinkConfiguration (*GetConfigurationPtr)(void *context);
 
 // Interface that exposes link functions to a plugin.
 struct LinkInterface {
-	GetNotificationPtr GetNotification;
-	CreatePluginWatchPtr CreatePluginWatch;
-	ConnectPtr Connect;
-	SendPtr Send;
-	GetInfoPtr GetInfo;
-	LogWritePtr LogWrite;
-	GetTotalRecvAvailablePtr GetTotalRecvAvailable;
-	GetRecvAvailablePtr GetRecvAvailable;
-	RecvPtr Recv;
-	CloseConnectionPtr CloseConnection;
-	CloseWatchPtr CloseWatch;
-	RegisterPluginPtr RegisterPlugin;
-	UnregisterPluginPtr UnregisterPlugin;
-	GetConfigurationPtr GetConfiguration;
+  GetNotificationPtr GetNotification;
+  CreatePluginWatchPtr CreatePluginWatch;
+  ConnectPtr Connect;
+  SendPtr Send;
+  GetInfoPtr GetInfo;
+  LogWritePtr LogWrite;
+  GetTotalRecvAvailablePtr GetTotalRecvAvailable;
+  GetRecvAvailablePtr GetRecvAvailable;
+  RecvPtr Recv;
+  CloseConnectionPtr CloseConnection;
+  CloseWatchPtr CloseWatch;
+  RegisterPluginPtr RegisterPlugin;
+  UnregisterPluginPtr UnregisterPlugin;
+  GetConfigurationPtr GetConfiguration;
 
-	void* context;
+  void *context;
 };
--- link/src/plugin/link_private.c
+++ link/src/plugin/link_private.c
@@ -7,36 +7,38 @@ static struct LinkInterface s_interface;
 /// call any of those functions from your plugin, which makes compiler
 /// strip those from shared object.
 
-int GetInterfaceVersion() {
-  return LINK_INTERFACE_VERSION;
-}
+int GetInterfaceVersion() { return LINK_INTERFACE_VERSION; }
 
-int StartupPlugin(struct LinkInterface* iface, const char* config, streamsize nbytes) {
-    s_interface = *iface;
-    return Startup(config, nbytes);
+int StartupPlugin(struct LinkInterface *iface, const char *config,
+                  streamsize nbytes) {
+  s_interface = *iface;
+  return Startup(config, nbytes);
 }
 
 struct LinkConfiguration GetConfiguration() {
   return s_interface.GetConfiguration(s_interface.context);
 }
 
-int GetNotification(struct Notification* notif, int timeout_ms) {
-    return s_interface.GetNotification(s_interface.context, notif, timeout_ms);
+int GetNotification(struct Notification *notif, int timeout_ms) {
+  return s_interface.GetNotification(s_interface.context, notif, timeout_ms);
 }
 
-WatchHandle CreateWatch(const char* name_match, const char* version_match, const char* hostname_match) {
-  return s_interface.CreatePluginWatch(s_interface.context, name_match ? name_match : "", version_match ? version_match : "", hostname_match ? hostname_match : "");
+WatchHandle CreateWatch(const char *name_match, const char *version_match,
+                        const char *hostname_match) {
+  return s_interface.CreatePluginWatch(
+      s_interface.context, name_match ? name_match : "",
+      version_match ? version_match : "", hostname_match ? hostname_match : "");
 }
 
-int GetPluginInfo(PluginHandle plugin, struct PluginInfo* info) {
-    return s_interface.GetInfo(s_interface.context, plugin, info);
+int GetPluginInfo(PluginHandle plugin, struct PluginInfo *info) {
+  return s_interface.GetInfo(s_interface.context, plugin, info);
 }
 
 ConnectionHandle Connect(PluginHandle handle) {
-    return s_interface.Connect(s_interface.context, handle);
+  return s_interface.Connect(s_interface.context, handle);
 }
 
-int Send(ConnectionHandle conn, const void* bytes, streamsize count) {
+int Send(ConnectionHandle conn, const void *bytes, streamsize count) {
   return s_interface.Send(s_interface.context, conn, bytes, count);
 }
 
@@ -48,7 +50,7 @@ unsigned int GetRecvAvailable(ConnectionHandle conn) {
   return s_interface.GetRecvAvailable(s_interface.context, conn);
 }
 
-int Recv(ConnectionHandle handle, void* buffer, unsigned int nbytes) {
+int Recv(ConnectionHandle handle, void *buffer, unsigned int nbytes) {
   return s_interface.Recv(s_interface.context, handle, buffer, nbytes);
 }
 
@@ -60,7 +62,7 @@ void CloseWatch(WatchHandle watch) {
   s_interface.CloseWatch(s_interface.context, watch);
 }
 
-PluginHandle RegisterPlugin(struct PluginInfo* info) {
+PluginHandle RegisterPlugin(struct PluginInfo *info) {
   return s_interface.RegisterPlugin(s_interface.context, info);
 }
 
@@ -68,14 +70,10 @@ void UnregisterPlugin(PluginHandle handle) {
   s_interface.UnregisterPlugin(s_interface.context, handle);
 }
 
-void LogWritePlugin(const char* file, int line, int category, const char* format, ...)  {
+void LogWritePlugin(const char *file, int line, int category,
+                    const char *format, ...) {
   va_list args;
   va_start(args, format);
-  s_interface.LogWrite(s_interface.context,
-                             file,
-                             line,
-                             category,
-                             format,
-                             args);
+  s_interface.LogWrite(s_interface.context, file, line, category, format, args);
   va_end(args);
 }
